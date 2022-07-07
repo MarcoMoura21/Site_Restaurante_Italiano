c2f2a4fa353a409b42af6ed2d02f31d90d32b8cd
MarcoMoura21 <mapmgi@hotmail.com>
2022-07-07 10:33:21 -0300

    Finalizacao

--- js/script.js
+++ js/script.js
@@ -6,9 +6,27 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
 }
 
+let section = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header .navbar a');
+
 window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+
+    section.forEach(sec =>{
+
+      let top = window.scrollY;
+      let height = sec.offsetHeight;
+      let offset = sec.offsetTop - 150;
+      let id = sec.getAttribute('id');
+
+      if(top >= offset && top < offset + height){
+        navlinks.forEach(links =>{
+          links.classList.remove('active');
+          document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+        });
+      };
+    });
 }
 
 document.querySelector('#search-icon').onclick = () => {
@@ -56,3 +74,13 @@ var swiper = new Swiper(".home-slider", {
       },
     },
   });
+
+function loader() {
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut() {
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
